76b1f247439b9b12d1dfa3936722f16f4e431e76
RoberDurango <rdurangolozano60@correo.unicordoba.edu.co>
2021-08-10 22:37:49 +0000

    Api basica

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 app.get("/", (req, res) =>{
-    res.send("HOLA MUNDO!")
+    res.send("HOLA MUNDO!!!")
 })
 app.listen(port, () =>{
     console.log("example app listening at http://localhost:${port}")
